5f06d054cfd5936f0085243c46f0fe09077d5b0a
jgutierrez18 <jepetoto18@gmail.com>
2023-04-21 18:48:38 -0500

    Primer commit DownTown

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
+import { DashboardModule } from './@admin/pages/dashboard/dashboard.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren:() => import('./@public/pages/home/home.module')
+          .then(m => m.HomeModule)
+  },
+  {
+    path: 'contact',
+    loadChildren:() => import('./@public/pages/contact/contact.module')
+          .then(m => m.ContactModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash: true,
+      scrollPositionRestoration: 'enabled'
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
