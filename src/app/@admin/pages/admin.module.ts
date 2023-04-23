7250502f28591e877c6e42e949abd6cb9f3820bc
jgutierrez18 <jepetoto18@gmail.com>
2023-04-23 12:04:04 -0500

    integracion Template a Admin, Sidebar, header, title, footer,

--- src/app/@admin/pages/admin.module.ts
+++ src/app/@admin/pages/admin.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { TitleComponent } from '../core/components/title/title.component';
+import { HeaderComponent } from '../core/components/header/header.component';
+import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
+import { FooterComponent } from '../core/components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    HeaderComponent, 
+    TitleComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
